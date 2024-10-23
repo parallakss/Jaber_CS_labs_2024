6e4e35ae559d664c831c511ca6c85d38cf431bdc
parallakss <ilya.jaber54@gmail.com>
2024-10-23 18:13:38 +0300

    lab3

--- variables.cpp
+++
@@ -1,9 +0,0 @@
-#include "variables.hpp"
-
-double permissibleError;
-int i{};
-double nextX{};
-double xN = 0.5;
-int iterationsCounter{};
-
-const double kMaxIterations = 1e5;
--- variables.hpp
+++
@@ -1,9 +0,0 @@
-extern double nextX;
-extern int i;
-extern double permissibleError;
-extern int iterationsCounter;
-
-extern double xN;
-extern const double kMaxIterations;
-
-double FirstTaskInputVariable();
